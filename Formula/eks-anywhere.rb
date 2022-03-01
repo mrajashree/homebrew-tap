a63a66a30d23ecd5dbd8d375a5d481fcfac42fe5
Rajashree Mandaogane <mandaor@amazon.com>
2022-03-01 11:06:15 -0800

    Update eks anywhere homebrew formula to v0.7.1

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -1,15 +1,15 @@
 class EksAnywhere < Formula
   desc "CLI for managing EKS Anywhere Kubernetes clusters"
   homepage "https://github.com/aws/eks-anywhere"
-  version "0.7.0"
+  version "0.7.1"
 
   if OS.mac?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/5/artifacts/eks-a/v0.7.0/darwin/amd64/eksctl-anywhere-v0.7.0-darwin-amd64.tar.gz"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/5/artifacts/eks-a/v0.7.1/darwin/amd64/eksctl-anywhere-v0.7.1-darwin-amd64.tar.gz"
     sha256 "1c918f513ed34506177d2b9afc1a7124ea42fa42c692fd4adfaf2d0494a1c1e6"
   end
 
   if OS.linux?
-    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/5/artifacts/eks-a/v0.7.0/linux/amd64/eksctl-anywhere-v0.7.0-linux-amd64.tar.gz"
+    url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/5/artifacts/eks-a/v0.7.1/linux/amd64/eksctl-anywhere-v0.7.1-linux-amd64.tar.gz"
     sha256 "2484f00bafdb860d418557fec5e7ab660647a0316ae541cdd58f0166a30cfa05"
   end
 
